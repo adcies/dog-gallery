ec5dcc7ee7897473cfa0197ca9a2bac3b6558848
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-09-27 15:41:06 +0200

    complete gallery, add button to load another image of the same, previously provided breed

--- src/classes/Gallery.js
+++ src/classes/Gallery.js
@@ -1,9 +1,15 @@
+import '../sass/Gallery.scss';
+
 class Gallery {
   constructor() {
+    this.breed = '';
+    this.subBreed = '';
     this.galleryWrapper = document.querySelector('.gallery');
   }
 
   createURL(breed, subBreed) {
+    this.breed = breed;
+    this.subBreed = subBreed ? subBreed : '';
     const url = subBreed
       ? `https://dog.ceo/api/breed/${breed}/${subBreed}/images/random`
       : `https://dog.ceo/api/breed/${breed}/images/random`;
@@ -33,16 +39,31 @@ class Gallery {
     this.getImageURL(url);
   }
 
+  createResetButton() {
+    const button = document.createElement('button');
+    button.classList.add('gallery__button', 'btn');
+    button.textContent = `Show another ${this.subBreed} ${this.breed}`.trim();
+    button.addEventListener('click', () => {
+      this.createGallery(`${this.subBreed} ${this.breed}`.trim());
+    });
+    return button;
+  }
+
   render(imgURL) {
     this.galleryWrapper.textContent = '';
     const imgWrapper = document.createElement('div');
+    const imgBackground = document.createElement('div');
     const img = document.createElement('img');
+    const button = this.createResetButton();
+    imgBackground.classList.add('gallery__img-background');
     imgWrapper.classList.add('gallery__img-wrapper');
     img.classList.add('gallery__img');
-    imgWrapper.style.backgroundImage = `url('${imgURL}')`;
+    imgBackground.style.backgroundImage = `url('${imgURL}')`;
     img.src = imgURL;
+    imgWrapper.appendChild(imgBackground);
     imgWrapper.appendChild(img);
     this.galleryWrapper.appendChild(imgWrapper);
+    this.galleryWrapper.appendChild(button);
   }
 }
 
